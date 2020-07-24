45b5af076470e01517620264717adaec101ed86a
Alex Nik <catalog.pri@gmail.com>
2020-07-24 10:12:06 +0300

    Added build folder

---
+++ build/precache-manifest.be6883e0c0f4593c2f01719ae080cd8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dda1b6ef987fa8303aceb64a378271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808d05015ece443f767f",
+    "url": "/static/css/main.712c2ec9.chunk.css"
+  },
+  {
+    "revision": "dd265b11ae6a45840361",
+    "url": "/static/js/2.7810e9a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7810e9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "808d05015ece443f767f",
+    "url": "/static/js/main.13a04020.chunk.js"
+  },
+  {
+    "revision": "76e0429abd4676778da5",
+    "url": "/static/js/runtime-main.61436ac2.js"
+  }
+]);
